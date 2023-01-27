e46945003ca63a0fcfb6df5a77a561874ade251c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-01-27 13:08:20 -0100

    created atuh module and register component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+const routes = [{
+    path: 'register',
+    component: RegisterComponent
+}]
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [RegisterComponent],
+  
+})
+export class AuthModule {}
