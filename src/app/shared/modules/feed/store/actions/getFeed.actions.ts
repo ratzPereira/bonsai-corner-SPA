11e2fe0bda734efb6c50cb4008982b2292e803b0
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2023-02-07 15:27:10 -0100

    created action types and get feed actions

---
+++ src/app/shared/modules/feed/store/actions/getFeed.actions.ts
@@ -0,0 +1,15 @@
+import { GetFeedResponseInterface } from './../../components/feed/types/get.feed.response.interface';
+import { ActionType } from './../actionTypes';
+import { createAction, props } from '@ngrx/store';
+
+export const getFeedAction = createAction(
+  ActionType.GET_FEED,
+  props<{ url: string }>()
+);
+
+export const getFeedSuccessAction = createAction(
+  ActionType.GET_FEED_SUCCESS,
+  props<{ feed: GetFeedResponseInterface }>()
+);
+
+export const getFeedFailureAction = createAction(ActionType.GET_FEED_FAILURE);
